7e7dfdf2b2a09c7d6d832834734f9e819dec11d6
Gerardo Lanza <glanza007@gmail.com>
2024-06-14 11:38:00 -0600

    full calendar

---
+++ EventPlanner/EventPlanner/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventPlanner.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+
+    }
+}
---
+++ EventPlanner/EventPlanner/Models/Evento.cs
@@ -0,0 +1,16 @@
+﻿namespace EventPlanner.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = null!;
+
+        public DateTime Fecha { get; set; }
+
+        public string Descripcion { get; set; } = null!;
+
+        public string Ubicacion { get; set;} = null!;
+
+    }
+}
